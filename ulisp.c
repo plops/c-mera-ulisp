9d012eed1177e842196e48a8c8aac8f60895752a
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-06-12 01:01:00 +0200

    add the c file

---
+++ ulisp.c
@@ -0,0 +1,193 @@
+#include <setjmp.h>
+#include <stdio.h>
+typedef unsigned int uint;
+typedef struct cons_object cons_object;
+
+struct cons_object
+{
+	cons_object *car;
+	cons_object *cdr;
+};
+typedef struct cons_symbol cons_symbol;
+
+struct cons_symbol
+{
+	unsigned int type;
+	unsigned int name;
+};
+typedef struct cons_number cons_number;
+
+struct cons_number
+{
+	unsigned int type;
+	int integer;
+};
+cons_object *freelist;
+cons_object *tee;
+cons_object *global_env;
+cons_object *gc_stack;
+unsigned int freespace;
+cons_object workspace[315];
+jmp_buf exception;
+char buffer[17 + 1];
+
+void init_workspace(void)
+{
+	freelist = 0;
+	for(int i = 315 - 1; 0 <= i; --i){
+		struct cons_object *obj = workspace + i;
+		((cons_object*)obj)->car = 0;
+		((cons_object*)obj)->cdr = freelist;
+		freelist = obj;
+		freespace++;
+	}
+}
+
+void erro(const char *string)
+{
+	printf("Error: %s\n", string);
+	gc_stack = 0;
+	longjmp(exception, 1);
+}
+
+cons_object *myalloc(void)
+{
+	if (0 == freespace) {
+		erro("No room");
+	}
+	cons_object *temp = freelist;
+	freelist = ((cons_object*)freelist)->cdr;
+	freespace--;
+	return temp;
+}
+
+void myfree(cons_object *obj)
+{
+	((cons_object*)obj)->cdr = freelist;
+	freelist = obj;
+	freespace++;
+}
+
+cons_object *make_number(int n)
+{
+	cons_number *ptr = ((cons_number*)myalloc());
+	ptr->type = 2;
+	ptr->integer = n;
+	return ((cons_object*)ptr);
+}
+
+cons_object *make_cons(cons_object *arg1, cons_object *arg2)
+{
+	cons_object *ptr = ((cons_object*)myalloc());
+	ptr->car = arg1;
+	ptr->cdr = arg2;
+	return ptr;
+}
+
+cons_object *make_csymbol(unsigned int name)
+{
+	cons_symbol *ptr = ((cons_symbol*)myalloc());
+	ptr->type = 1;
+	ptr->name = name;
+	return ((cons_object*)ptr);
+}
+
+void mark_object(cons_object *obj)
+{
+	if (0 == obj) {
+		return;
+	}
+	if (0 != (((uint)((cons_object*)obj)->car) & 32768)) {
+		return;
+	}
+	cons_object *arg = ((cons_object*)obj)->car;
+	int type = ((cons_number*)obj)->type;
+	((cons_object*)obj)->car = ((uint)((cons_object*)obj)->car) | 32768;
+	if ((1 != type) && (2 != type)) {
+		mark_object(arg);
+		mark_object(((cons_object*)obj)->cdr);
+	}
+}
+
+void sweep(void)
+{
+	freelist = 0;
+	freespace = 0;
+	for(int i = 315 - 1; 0 <= i; --i){
+		cons_object *obj = workspace + i;
+		if (1 == (0 != (((uint)((cons_object*)obj)->car) & 32768))) {
+			((cons_object*)obj)->car = ((uint)((cons_object*)obj)->car) & 32767;
+		}
+		else {
+			((cons_object*)obj)->car = ((cons_object*)0);
+			((cons_object*)obj)->cdr = freelist;
+			freelist = obj;
+			freespace++;
+		}
+	}
+}
+
+void gc(cons_object *form, cons_object *env)
+{
+	mark_object(tee);
+	mark_object(global_env);
+	mark_object(gc_stack);
+	mark_object(form);
+	mark_object(env);
+	sweep();
+}
+
+int toradix40(int ch)
+{
+	if (0 == ch) {
+		return 0;
+	}
+	if (('0' <= ch) && (ch <= '9')) {
+		return 30 + (ch - '0');
+	}
+	ch = ch | 32;
+	if (('a' <= ch) && (ch <= 'z')) {
+		return 1 + (ch - 'a');
+	}
+	erro("ill. char in sym");
+	return 0;
+}
+
+int fromradix40(int n)
+{
+	if ((1 <= n) && (n <= 26)) {
+		return n + 'a' + -1;
+	}
+	if ((1 <= 30) && (n <= 39)) {
+		return n + '0' + -30;
+	}
+	if (27 == n) {
+		return '-';
+	}
+	return 0;
+}
+
+int pack40(char *c)
+{
+	return (40 * ((40 * toradix40(c[0])) + toradix40(c[1]))) + toradix40(c[2]);
+}
+
+int digitvalue(char d)
+{
+	if (('0' <= d) && (d <= '9')) {
+		return d - '0';
+	}
+	d = d | 32;
+	if (('a' <= d) && (d <= 'f')) {
+		return 10 + (d - 'a');
+	}
+	return 16;
+}
+
+char *name(cons_object *obj)
+{
+	buffer[3] = ((char)0);
+	if (1 != ((cons_symbol*)obj)->type) {
+		erro("name");
+	}
+}
