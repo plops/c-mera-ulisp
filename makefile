4dfbb1250a0e5ba2518950fee87878b8324fe6f0
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-06-16 11:38:43 +0200

    rename cons_object* to o, then it looks nicer in ddd debugger

--- makefile
+++ makefile
@@ -1,6 +1,9 @@
 ulisp: ulisp.c
-	gcc -Og -Wall -Wextra ulisp.c -o ulisp
+	gcc -Og -ggdb3 -pg --coverage -fno-omit-frame-pointer -ffloat-store -fno-common -fstrict-aliasing -Wswitch-default -Wfloat-equal -Winline -Wundef -Wnested-externs  -Wstrict-aliasing=3 -Wall -Wextra -pedantic -std=c99 -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes  ulisp.c  -o ulisp
 
 ulisp.s: ulisp.c
 	gcc -S -Wall -Wextra -O2  ulisp.c
 	cat ulisp.s
+
+clean:
+	rm ulisp
--- ulisp.lisp
+++ ulisp.lisp
@@ -33,7 +33,7 @@
 
 (defmacro mark (x)
   `(set (cons-car ,x)
-	(cast 'cons_object*
+	(cast 'o
 	      (\| (cast 'uintgr (cons-car ,x))
 		  ;; #x8000
 		  *mark-bit*
@@ -41,7 +41,7 @@
 
 (defmacro unmark (x)
   `(set (cons-car ,x)
-	(cast 'cons_object*
+	(cast 'o
 	      (& (cast 'uintgr (cons-car ,x))
 		 ;; #x7fff
 		 (- *mark-bit* 1)
@@ -54,9 +54,9 @@
 (defparameter *number* 2)
 
 (defmacro cons-car (x)
-  `(pref (cast '(struct cons_object*) ,x) car))
+  `(pref (cast 'o ,x) car))
 (defmacro cons-cdr (x)
-  `(pref (cast '(struct cons_object*) ,x) cdr))
+  `(pref (cast 'o ,x) cdr))
 (defmacro cons-name (x)
   `(pref (cast '(struct cons_symbol*) ,x) name))
 (defmacro cons-type (x)
@@ -216,21 +216,21 @@ and throws error when string is not a builtin."
 
 (defmacro defspecial (name &body body)
   `(function ,(intern (string-upcase (format nil "sp_~a" name)))
-       ((cons_object* args)
-	(cons_object* env))
-       -> cons_object*
+       ((o args)
+	(o env))
+       -> o
      ,@body))
 
 (defmacro deftailrec (name &body body)
-  `(function ,(intern (string-upcase (format nil "tf_~a" name))) ((cons_object* args)
-								  (cons_object* env))
-       -> cons_object*
+  `(function ,(intern (string-upcase (format nil "tf_~a" name))) ((o args)
+								  (o env))
+       -> o
      ,@body))
 
 (defmacro deffunction (name &body body)
-  `(function ,(intern (string-upcase (format nil "fn_~a" name))) ((cons_object* args)
-								  (cons_object* env))
-       -> cons_object*
+  `(function ,(intern (string-upcase (format nil "fn_~a" name))) ((o args)
+								  (o env))
+       -> o
      ,@body))
 
 (defmacro ensure-symbol (var)
@@ -254,6 +254,7 @@ and throws error when string is not a builtin."
 (defparameter *quo* 3)
 (defparameter *dot* 4)
 
+#+nil
 (let ((workspace-size 315)
       (buflen (builtin-function-name-maxlength)) ;; length of longest symbol 
       (cnil 'NULL))
@@ -289,10 +290,10 @@ and throws error when string is not a builtin."
 					 buflen)
 				  (builtin-function-name-clist))
 			   ))
-		    (decl ((cons_object* freelist)
-			   (cons_object* tee)
-			   (cons_object* global-env)
-			   (cons_object* gc-stack)
+		    (decl ((o freelist)
+			   (o tee)
+			   (o global-env)
+			   (o gc-stack)
 			   (uintgr freespace)
 			   (cons_object (aref workspace workspace-size))
 			   (jmp_buf exception)
@@ -317,13 +318,13 @@ and throws error when string is not a builtin."
 				   last-char)
 		    (comment "forward declarations")
 		    (deftailrec progn) (comment ";" :prefix "")
-		    (function _eval ((cons_object* form)(cons_object* env)) -> cons_object*) (comment ";" :prefix "")
+		    (function _eval ((o form)(o env)) -> o) (comment ";" :prefix "")
 		    (function _read () -> o) (comment ";" :prefix "")
 		    (function init-workspace () -> void
 		      (set freelist 0)
 		      (for ((intgr i (- workspace-size 1))
 			    (<= 0 i) (dec i))
-			(decl ((struct cons_object* obj (+ workspace i)))
+			(decl ((o obj (+ workspace i)))
 			  (set (cons-car obj) 0)
 			  (set (cons-cdr obj) freelist)
 			  (set freelist obj)
@@ -332,46 +333,46 @@ and throws error when string is not a builtin."
 		      (funcall printf "Error: %s\\n" string)
 		      (set gc-stack 0)
 		      (funcall longjmp exception 1))
-		    (function _alloc () -> cons_object*
+		    (function _alloc () -> o
 		      (if (== 0 freespace)
 			  (funcall erro "No room"))
-		      (decl ((cons_object* temp freelist))
+		      (decl ((o temp freelist))
 			(set freelist (cons-cdr freelist))
 			(set freespace (- freespace 1))
 			(return temp)))
-		    (function myfree ((cons_object* obj)) -> void
+		    (function myfree ((o obj)) -> void
 		      (set (cons-cdr obj) freelist)
 		      (set freelist obj)
 		      (inc freespace))
-		    (function  _number ((intgr n)) -> cons_object*
+		    (function  _number ((intgr n)) -> o
 		      (decl ((cons_number* ptr
 					   (cast 
 					    'cons_number*
 					    (funcall _alloc))))
 			(set (pref ptr type) *number*)
 			(set (pref ptr integer) n)
-			(return (cast cons_object* ptr))))
-		    (function _cons ((cons_object* arg1)
-				     (cons_object* arg2)) -> cons_object*
-		      (decl ((cons_object* ptr (cast 'cons_object*
+			(return (cast o ptr))))
+		    (function _cons ((o arg1)
+				     (o arg2)) -> o
+		      (decl ((o ptr (cast 'o
 						     (funcall _alloc))))
 			(set (pref ptr car) arg1)
 			(set (pref ptr cdr) arg2)
 			(return ptr)))
-		    (function _symbol ((uintgr name)) -> cons_object*
+		    (function _symbol ((uintgr name)) -> o
 		      (decl ((cons_symbol* ptr
 					   (cast 
 					    'cons_symbol*
 					    (funcall _alloc))))
 			(set (pref ptr type) *symbol*)
 			(set (pref ptr name) name)
-			(return (cast cons_object* ptr))))
-		    (function mark-object ((cons_object* obj)) -> void
+			(return (cast o ptr))))
+		    (function mark-object ((o obj)) -> void
 		      (if (== 0 obj)
 			  (return))
 		      (if (marked obj)
 			  (return))
-		      (decl ((cons_object* arg (cons-car obj))
+		      (decl ((o arg (cons-car obj))
 			     (intgr type (pref (cast 'cons_number* obj)
 					       type)))
 			(mark obj)
@@ -384,15 +385,15 @@ and throws error when string is not a builtin."
 		      (set freelist 0)
 		      (set freespace 0)
 		      (for ((int i (- workspace-size 1)) (<= 0 i) (dec i))
-			(decl ((cons_object* obj (+ workspace i)))
+			(decl ((o obj (+ workspace i)))
 			  (if (== 1 (marked obj))
 			      (unmark obj)
 			      (progn
-				(set (cons-car obj) (cast 'cons_object* 0))
+				(set (cons-car obj) (cast 'o 0))
 				(set (cons-cdr obj) freelist)
 				(set freelist obj)
 				(inc freespace))))))
-		    (function gc ((cons_object* form) (cons_object* env)) -> void
+		    (function gc ((o form) (o env)) -> void
 		      (mark-object tee)
 		      (mark-object global-env)
 		      (mark-object gc-stack)
@@ -435,7 +436,7 @@ and throws error when string is not a builtin."
 		      (for ((int i 0) (< i buflen) (inc i))
 			(set (aref buffer i) (aref builtin-name name i)))
 		      (return buffer))
-		    (function name ((cons_object* obj)) -> char*
+		    (function name ((o obj)) -> char*
 		      (set (aref buffer 3) (cast 'char 0))
 		      (if (!= *symbol* (cons-type obj))
 			  (erro "name"))
@@ -446,14 +447,14 @@ and throws error when string is not a builtin."
 			  (set (aref buffer n) (funcall fromradix40 (% x 40)))
 			  (set x (/ x 40))))
 		      (return buffer))
-		    (function _integer ((cons_object* obj)) -> intgr
+		    (function _integer ((o obj)) -> intgr
 		      (if (!= *number* (cons-type obj))
 			  (erro "not number"))
 		      (return (cons-integer obj)))
-		    (function issymbol ((cons_object* obj) (uintgr n)) -> int
+		    (function issymbol ((o obj) (uintgr n)) -> int
 		      (return (and (== *symbol* (cons-type obj))
 				   (cons-name n))))
-		    (function _eq ((cons_object* a) (cons_object* b)) -> int
+		    (function _eq ((o a) (o b)) -> int
 		      (return (or
 				(== a b)
 				(and (== *symbol* (cons-type a))
@@ -462,37 +463,37 @@ and throws error when string is not a builtin."
 				(and (== *number* (cons-type a))
 				     (== *number* (cons-type b))
 				     (== (cons-integer a) (cons-integer b))))))
-		    (function value ((uintgr n) (cons_object* env)) -> cons_object*
+		    (function value ((uintgr n) (o env)) -> o
 		      (while (!= NULL env)
-			(decl ((cons_object* item (cons-car env)))
+			(decl ((o item (cons-car env)))
 			  (if (== n (cons-name (cons-car item)))
 			      (return item))
 			  (set env (cons-cdr env))))
 		      (return cnil))
-		    (function findvalue ((cons_object* var) (cons_object* env)) -> cons_object*
+		    (function findvalue ((o var) (o env)) -> o
 		      (decl ((uintgr varname (cons-name var))
-			     (cons_object* pair (funcall value varname env)))
+			     (o pair (funcall value varname env)))
 			(if (== NULL pair)
 			    (set pair (funcall value varname global-env)))
 			(if (== NULL pair)
 			    (erro "unknown var"))
 			(return pair)))
-		    (function findtwin ((cons_object* var) (cons_object* env)) -> cons_object*
+		    (function findtwin ((o var) (o env)) -> o
 		      (while (!= NULL env)
-			(decl ((cons_object* item (cons-car env)))
+			(decl ((o item (cons-car env)))
 			  (if (== var (cons-car item))
 			      (return item))
 			  (set env (cons-cdr env))))
 		      (return cnil))
 		    
 		    (function closure ((int tail)
-				       (cons_object* fname)
-				       (cons_object* state)
-				       (cons_object* function)
-				       (cons_object* args)
-				       (cons_object** env)) -> cons_object*
+				       (o fname)
+				       (o state)
+				       (o function)
+				       (o args)
+				       (o* env)) -> o
 		      (comment "(void) fname;" :prefix "")
-		      (decl ((cons_object* params (cons-car function)))
+		      (decl ((o params (cons-car function)))
 			(set function (cons-cdr function))
 			(comment "push state if not already in env")
 			(while (!= NULL state)
@@ -507,7 +508,7 @@ and throws error when string is not a builtin."
 			  (decl ((o var (cons-car params))
 				 (o value (cons-car args)))
 			    (if tail
-				(decl ((cons_object* item (funcall findtwin var *env)))
+				(decl ((o item (funcall findtwin var *env)))
 				  (if (!= NULL item)
 				      (set (cons-cdr item) value)
 				      (_push (funcall _cons var value) *env)))
@@ -520,7 +521,7 @@ and throws error when string is not a builtin."
 			    (erro "too many params"))
 			(comment "do implicit progn")
 			(return (funcall tf-progn function *env))))
-		    (function listlength ((cons_object* list)) -> int
+		    (function listlength ((o list)) -> int
 		      (decl ((int len 0))
 			(while (!= NULL list)
 			  (set list (cons-cdr list))
@@ -543,9 +544,9 @@ and throws error when string is not a builtin."
 			   (aref builtin-fptr (cl:length *builtin-function*)))))
 		    (function lookupfn ((uintgr name)) -> fn_ptr_type
 		      (return (aref builtin-fptr name)))
-		    (function _apply ((cons_object* function)
-				      (cons_object* args)
-				      (cons_object** env)) -> cons_object*
+		    (function _apply ((o function)
+				      (o args)
+				      (o* env)) -> o
 		      (if (== *symbol* (cons-type function))
 			  (decl ((uintgr name (cons-name function))
 				 (int nargs (funcall listlength args)))
@@ -565,7 +566,7 @@ and throws error when string is not a builtin."
 					 'lambda)))
 			  (progn
 			    (set function (cons-cdr function))
-			    (decl ((cons_object*
+			    (decl ((o
 				    result
 				    (funcall closure 0
 					     NULL NULL function args env)))
@@ -576,7 +577,7 @@ and throws error when string is not a builtin."
 					 'closure)))
 			  (progn
 			    (set function (cons-cdr function))
-			    (decl ((cons_object*
+			    (decl ((o
 				    result
 				    (funcall closure 0
 					     NULL
@@ -587,13 +588,13 @@ and throws error when string is not a builtin."
 		      (erro "illegal function")
 		      (return NULL))
 		    (comment "checked car and cdr")
-		    (function carx ((cons_object* arg)) -> cons_object*
+		    (function carx ((o arg)) -> o
 		      (if (== 0 (_listp arg))
 			  (erro "can't take car"))
 		      (if (== cnil arg)
 			  (return cnil))
 		      (return (cons-car arg)))
-		    (function cdrx ((cons_object* arg)) -> cons_object*
+		    (function cdrx ((o arg)) -> o
 		      (if (== 0 (_listp arg))
 			  (erro "can't take cdr"))
 		      (if (== cnil arg)
@@ -604,9 +605,9 @@ and throws error when string is not a builtin."
 		      (return (cons-car args)))
 		    (defspecial defun
 		      (comment "(void) env;" :prefix "")
-		      (decl ((cons_object* var (cons-car args)))
+		      (decl ((o var (cons-car args)))
 			(ensure-symbol var)
-			(decl ((cons_object*
+			(decl ((o
 				val
 				(funcall
 				 _cons
@@ -614,7 +615,7 @@ and throws error when string is not a builtin."
 				  _symbol
 				  (builtin-function-name-to-number 'lambda))
 				 (cons-cdr args)))
-			       (cons_object* pair
+			       (o pair
 					     (funcall value (cons-name var)
 						      global-env)))
 			  (if (!= NULL pair)
@@ -623,11 +624,11 @@ and throws error when string is not a builtin."
 			  (_push (funcall _cons var val) global-env)
 			  (return var))))
 		    (defspecial defvar
-		      (decl ((cons_object* var (cons-car args)))
+		      (decl ((o var (cons-car args)))
 			(ensure-symbol var)
-			(decl ((cons_object* val (funcall _eval (_second args)
+			(decl ((o val (funcall _eval (_second args)
 							  env))
-			       (cons_object* pair (funcall value (cons-name var)
+			       (o pair (funcall value (cons-name var)
 							   global-env)))
 			  (if (!= NULL pair)
 			      (set (cons-cdr pair) val)
@@ -636,41 +637,41 @@ and throws error when string is not a builtin."
 				 global-env)
 			  (return var))))
 		    (defspecial setq
-		      (decl ((cons_object* arg (funcall _eval
+		      (decl ((o arg (funcall _eval
 							(_second args)
 							env))
-			     (cons_object* pair (funcall findvalue
+			     (o pair (funcall findvalue
 							 (cons-car args)
 							 env)))
 			(set (cons-cdr pair) arg)
 			(return arg)))
 		    (defspecial loop
 		      (set return-flag 0)
-		      (decl ((cons_object* start args))
+		      (decl ((o start args))
 			(for (() () ())
 			  (set args start)
 			  (while (!= NULL args)
-			    (decl ((cons_object* form (cons-car args))
-				   (cons_object* result (funcall _eval form env)))
+			    (decl ((o form (cons-car args))
+				   (o result (funcall _eval form env)))
 			      (if (== 1 return-flag)
 				  (set return-flag 0)
 				  (return result)))
 			    (set args (cons-cdr args))))))
 		    (defspecial push
-		      (decl ((cons_object* item (funcall _eval (cons-car args) env))
-			     (cons_object* pair (funcall findvalue (_second args)
+		      (decl ((o item (funcall _eval (cons-car args) env))
+			     (o pair (funcall findvalue (_second args)
 							 env)))
 			(_push item (cons-cdr pair))
 			(return (cons-cdr pair))))
 		    (defspecial pop
-		      (decl ((cons_object* pair (funcall findvalue (_second args)
+		      (decl ((o pair (funcall findvalue (_second args)
 							 env))
-			     (cons_object* result (cons-car (cons-cdr pair))))
+			     (o result (cons-car (cons-cdr pair))))
 			(_pop (cons-cdr pair))
 			(return result)))
 		    (defspecial incf
-		      (decl ((cons_object* var (cons-car args))
-		    	     (cons_object* pair (funcall findvalue var env))
+		      (decl ((o var (cons-car args))
+		    	     (o pair (funcall findvalue var env))
 		    	     (int result (funcall _integer (funcall _eval var env)))
 		    	     (int temp 1))
 		    	(if (!= NULL (cons-cdr args))
@@ -680,8 +681,8 @@ and throws error when string is not a builtin."
 		    	(set (cons-cdr pair) var)
 		    	(return var)))
 		    (defspecial decf
-		      (decl ((cons_object* var (cons-car args))
-		    	     (cons_object* pair (funcall findvalue var env))
+		      (decl ((o var (cons-car args))
+		    	     (o pair (funcall findvalue var env))
 		    	     (int result (funcall _integer (funcall _eval var env)))
 		    	     (int temp 1))
 		    	(if (!= NULL (cons-cdr args))
@@ -694,7 +695,7 @@ and throws error when string is not a builtin."
 		    (deftailrec progn
 		      (if (== NULL args)
 		    	  (return cnil))
-		      (decl ((cons_object* more (cons-cdr args)))
+		      (decl ((o more (cons-cdr args)))
 		    	(while (!= NULL more)
 		    	  (funcall _eval (cons-car args) env)
 		    	  (set args more)
@@ -788,8 +789,8 @@ and throws error when string is not a builtin."
 					 (cons-cdr args)
 					 (addr-of env)))))
 		    
-		    (function _eval ((cons_object* form)
-				     (cons_object* env)) -> cons_object*
+		    (function _eval ((o form)
+				     (o env)) -> o
 		      (decl ((int TC 0))
 			(comment "EVAL:" :prefix "") ;; FIXME this is crazy
 			(if (< freespace 10)
@@ -804,7 +805,7 @@ and throws error when string is not a builtin."
 			      (if (== (builtin-function-name-to-number 'nil)
 				      name)
 				  (return cnil))
-			      (decl ((cons_object* pair (funcall value name env)))
+			      (decl ((o pair (funcall value name env)))
 				(if (!= NULL pair)
 				    (return (cons-cdr pair)))
 				(set pair (value name global-env))
@@ -814,8 +815,8 @@ and throws error when string is not a builtin."
 					(return form))))
 			      (erro "undefined")))
 			(comment "it's a list")
-			(decl ((cons_object* function (cons-car form))
-			       (cons_object* args (cons-cdr form)))
+			(decl ((o function (cons-car form))
+			       (o args (cons-cdr form)))
 			  (comment "list starting with symbol?")
 			  (if (== *symbol* (cons-type function))
 			      (decl ((uintgr name (cons-name function)))
@@ -824,11 +825,11 @@ and throws error when string is not a builtin."
 					name)
 				    ;; FIXME leaving out LETSTAR
 				    
-				    (decl ((cons_object* assigns (cons-car args))
-					   (cons_object* forms (cons-cdr args))
-					   (cons_object* newenv env))
+				    (decl ((o assigns (cons-car args))
+					   (o forms (cons-cdr args))
+					   (o newenv env))
 				      (while (!= NULL assigns)
-					(decl ((cons_object* assign (cons-car assigns)))
+					(decl ((o assign (cons-car assigns)))
 					  (if (_consp assign)
 					      (_push (_cons (cons-car assign)
 							    (funcall _eval
@@ -847,10 +848,10 @@ and throws error when string is not a builtin."
 				    (progn
 				      (if (== NULL env)
 					  (return form))
-				      (decl ((cons_object* envcopy NULL))
+				      (decl ((o envcopy NULL))
 					(while (!= NULL env)
-					  (decl ((cons_object* pair (cons-car env))
-						 (cons_object* val (cons-cdr pair)))
+					  (decl ((o pair (cons-car env))
+						 (o val (cons-cdr pair)))
 					    (if (== *number* (cons-type val))
 						(set val (funcall _number
 								  (cons-integer val))))
@@ -881,20 +882,20 @@ and throws error when string is not a builtin."
 				      (set TC 1)
 				      (comment "goto EVAL;" :prefix "")))))
 			  (comment "evaluate the parameters - result in head")
-			  (decl ((cons_object* fname (cons-car form))
+			  (decl ((o fname (cons-car form))
 				 (int TCstart TC)
-				 (cons_object* head (funcall _cons
+				 (o head (funcall _cons
 							     (funcall _eval
 								      (cons-car form)
 								      env)
 							     NULL)))
 			    (comment "don't gc the result list")
 			    (_push head gc-stack) 
-			    (decl ((cons_object* tail head))
+			    (decl ((o tail head))
 			      (set form (cons-cdr head))
 			      (decl ((int nargs 0))
 				(while (!= NULL form)
-				  (decl ((cons_object* obj (funcall _cons
+				  (decl ((o obj (funcall _cons
 								    (funcall _eval (cons-car form) env)
 								    NULL)))
 				    (set (cons-cdr tail) obj)
@@ -1028,6 +1029,7 @@ and throws error when string is not a builtin."
 				  (return (funcall _symbol x))
 				  (return (funcall _symbol (funcall pack40 buffer)))))))))
 		    (function read-rest () -> o
+		      (funcall printf "read-rest\\n")
 		      (decl ((o item (funcall nextitem)))
 			(if (== (cast 'o *ket*) item)
 			    (return NULL))
@@ -1098,6 +1100,7 @@ and throws error when string is not a builtin."
 				     (return)))
 			  (funcall printf "\\n")
 			  (_push line gc-stack)
+			  (funcall printf "push line gc-stack\\n")
 			  (funcall _print-object (funcall _eval line env))
 			  (_pop gc-stack)
 			  (funcall printf "\\n\\n")
