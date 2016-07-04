a78480639b0693e8461003ea86c0aa3b2c4923b8
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-07-05 00:06:49 +0200

    find out which functions are necessary for the route finder

--- makefile
+++ makefile
@@ -3,9 +3,9 @@ DEADCODESTRIP := #-Wl,-static -fdata-sections -ffunction-sections -Wl,--gc-secti
 #-Wl,-s
 WARN := -Wswitch-default -Wfloat-equal -Winline -Wundef -Wnested-externs  -Wstrict-aliasing=3 -Wall -Wextra -pedantic -std=c99 -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes -Wshift-negative-value -Wshift-overflow=2 -Wtautological-compare -Wnull-dereference -Wduplicated-cond  -Wmisleading-indentation 
 
-#CFLAGS := -Os -fomit-frame-pointer #-fno-omit-frame-pointer -ffloat-store -fno-common -fstrict-aliasing
+CFLAGS := -Os -fomit-frame-pointer #-fno-omit-frame-pointer -ffloat-store -fno-common -fstrict-aliasing
 
-CFLAGS := -Og -ggdb3 -fno-omit-frame-pointer -ffloat-store -fno-common -fstrict-aliasing -fsanitize=address -fsanitize=bounds-strict -march=native -fdelete-null-pointer-checks  -fsanitize=undefined
+#CFLAGS := -Og -ggdb3 -fno-omit-frame-pointer -ffloat-store -fno-common -fstrict-aliasing -fsanitize=address -fsanitize=bounds-strict -march=native -fdelete-null-pointer-checks  -fsanitize=undefined
 
 ulisp-interp: ulisp.c
 	gcc  $(CFLAGS) $(DEADCODESTRIP) $(WARN)  ulisp.c  -o ulisp-interp
--- tak.lisp
+++ tak.lisp
@@ -21,3 +21,43 @@
 (defun bla (a) (setq a (add a -1)) (if (less 0 a) 1 0))
 (bla 2)
 
+
+;; list null dolist assoc
+(defvar md nil)
+
+(defun adr (frm to tim) ;; add road
+  (push (list frm to tim) md)
+  (push (list to frm tim) md))
+
+(defun adi (i q) ;; add item to queue
+  (if (null q) 
+      (cons i q)
+    (if (< (first i) (car (car q)))
+        (cons i q)
+      (cons (first q) (adi i (cdr q))))))
+
+(defun adq (tim loc via pq) ;; add to queue
+  (setq pq (adi (list tim loc via) pq))
+  pq)
+
+(defun ars (loc go pq) ;; add roads
+  (dolist (i md pq)
+    (let* ((frm (first i))
+           (to (second i))
+           (tim (third i)))
+      (when (and (eq frm loc) (not (assoc to vis)))
+	(setq pq (adq (add go tim) to loc pq))))))
+
+(defun gro (frm to)
+  (let* ((vis (list (cons frm nil)))
+         (pq (ars frm 0 nil))
+         w)
+    (loop
+     (when (eq frm to) (return (reverse vis)))
+     (unless pq (return))
+     (setq w (first pq))
+     (setq frm (second w))
+     (setq pq (cdr pq))
+     (unless (assoc frm vis)
+       (setq vis (cons (cons frm (third w)) vis))
+       (setq pq (ars frm (car w) pq))))))
